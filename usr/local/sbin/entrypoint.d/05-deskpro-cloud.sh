1f3010063909b3172011e0e42d4c543d02d6d50f
MattGrundy <38431311+MattGrundy@users.noreply.github.com>
2024-06-05 17:08:58 +0100

    Add cloud init script Adjust to pick up deskpro site info envs in cloud leveraging the app

---
+++ usr/local/sbin/entrypoint.d/05-deskpro-cloud.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#######################################################################
+# This source handles setup steps for Deskpro Cloud.
+#######################################################################
+
+function deskpro_cloud_main() {
+  if [ -z "$DESKPRO_CLOUD_MODE" ]; then
+    boot_log_message TRACE "[cloud] Not running in Cloud mode, skipping cloud setup."
+    return
+  fi
+  cp -r /srv/deskpro/cloud/. /srv/deskpro/INSTANCE_DATA
+  echo "set nocompatible" >> /etc/vim/vimrc.local
+
+}
+
+deskpro_cloud_main
+unset deskpro_cloud_main
