403aa9472b6d823839e0c7620c11355d3535985a
Felipe Bessa Coelho <felipe.coelho@deskpro.com>
2025-04-15 12:28:35 +0100

    enable php-fpm slow logs

--- usr/local/sbin/entrypoint.d/00-env.sh
+++ usr/local/sbin/entrypoint.d/00-env.sh
@@ -22,6 +22,7 @@ for f in \
   "/var/log/nginx/error.log" \
   "/var/log/php/error.log" \
   "/var/log/php/fpm_error.log" \
+  "/var/log/php/fpm_slow.log" \
   "/var/log/supervisor/tasks.log" \
   "/var/log/supervisor/email_collect.log" \
   "/var/log/supervisor/email_process.log" \
